80391d38213d22b640922bf10ac579ed6d85b5a7
thminh176 <thminh.176.vn@gmail.com>
2024-10-11 01:24:37 +0700

    Cập nhật cấu hình để xử lý port

--- api/server.js
+++ api/server.js
@@ -5,6 +5,13 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 3000, () => {
+
+// Nếu PORT không được thiết lập, gán giá trị mặc định là 3000
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+// In ra giá trị cổng để kiểm tra
+console.log("Server is starting on port:", port);
+
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
